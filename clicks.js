8e8b13f443b4908b48d91a6245ef7bab55120d41
jobohan <jobohan@s.sfusd.edu>
2015-02-24 17:34:33 -0800

    h
    h

--- clicks.js
+++ clicks.js
@@ -8,18 +8,9 @@ $("h1.page-title").text("My code runs!");
 
 
 $(window).on('click', function (e) {
-    $('img.logo').css({left: e.pageX }); });
-
-
-$(window).on('click', function (e) {
-    $('img.logo').css({left: e.pageX,
-                       top: e.pageY}); });
-
-
-$(window).on('click', function (e) {
-    $('img.logo').css({left: e.pageX,
+    $('.logo').css({left: e.pageX,
                     top: e.pageY});
-    $('img.logo').toggleClass('rotated');
+    $('.logo').toggleClass('rotated');
 });
 
 
