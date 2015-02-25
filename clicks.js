4621536abb83a5b6dc36718aa33669e4befcc2a9
jobohan <jobohan@s.sfusd.edu>
2015-02-24 17:47:41 -0800

    h
    h

--- clicks.js
+++ clicks.js
@@ -54,3 +54,52 @@ setInterval(function () {
     x = x + vx;
     y = y + vy;
 }, 20);
+
+var vx = Math.random() * 10 - 5;
+var vy = Math.random() * 10 - 5;
+var x = $(window).width() / 2;
+var y = $(window).height() / 2;
+
+setInterval(function () {
+  $('.logo').css({
+      left: x,
+      top: y
+  });
+  x = x + vx;
+  y = y + vy;
+  if (x < 0) {
+      x = 0;
+      vx = -vx;
+  } else if (x > $(window).width()) {
+      x = $(window).width();
+      vx = -vx;
+  }
+}, 20);
+
+var vx = Math.random() * 10 - 5;
+var vy = Math.random() * 10 - 5;
+var x = $(window).width() / 2;
+var y = $(window).height() / 2;
+
+setInterval(function () {
+  $('.logo').css({
+      left: x,
+      top: y
+  });
+  x = x + vx;
+  y = y + vy;
+  if (x < 0) {
+      x = 0;
+      vx = -vx;
+  } else if (x > $(window).width()) {
+      x = $(window).width();
+      vx = -vx;
+  }
+  if (y < 0) {
+      y = 0;
+      vy = -vy;
+  } else if (y > $(window).height()) {
+      y = $(window).height();
+      vy = -vy;
+  }
+}, 20);
