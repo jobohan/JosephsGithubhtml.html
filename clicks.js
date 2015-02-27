036136f4a2185729b168b03fa69258d6c6269308
jobohan <jobohan@s.sfusd.edu>
2015-02-26 17:40:08 -0800

    h
    h

--- clicks.js
+++ clicks.js
@@ -4,16 +4,22 @@
 /*global $*/
 /*jslint sloppy:true, browser: true, white: true*/
 
-$("h1.page-title").text("sun");
+$("h1.page-title").text("mercury");
+
+var x = $(window).width() / 2;
+var y = $(window).height() / 2;
+var theta = 0;
+var vtheta = Math.PI / 180;
+var r = 200;
+setInterval(function () {
+    $('.mercury').css({
+        'left': x + r * Math.cos(theta),
+        'top': y + r * Math.sin(theta)
+    });
+    theta = theta + vtheta;
+}, 20);
 
 
-$(window).on('mousemove', function (e) {
-    $('img.sun').css({left: e.pageX }); });
-
-
-$(window).on('mousemove', function (e) {
-    $('img.sun').css({left: e.pageX,
-                       top: e.pageY}); });
 
 
 
