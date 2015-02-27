2f1d39eea3dce045620ac64e5825f7da6588b673
jobohan <jobohan@s.sfusd.edu>
2015-02-26 17:57:50 -0800

    h
    h

--- clicks.js
+++ clicks.js
@@ -4,13 +4,22 @@
 /*global $*/
 /*jslint sloppy:true, browser: true, white: true*/
 
+$(window).on('click', function (e) {
+    $('sun').css({left: e.pageX});
+});
+
+$(window).on('click', function (e) {
+    $('sun').css({left: e.pageXXX,
+                       top: e.pageYYY});
+});
+
 $("h1.page-title").text("mercury");
 
 var x = $(window).width() / 2;
 var y = $(window).height() / 2;
 var theta = 0;
-var vtheta = Math.PI / 180;
-var r = 200;
+var vtheta = Math.PI / 400;
+var r = 100;
 setInterval(function () {
     $('.mercury').css({
         'left': x + r * Math.cos(theta),
@@ -21,5 +30,17 @@ setInterval(function () {
 
 
 
+var xx = $(window).width() / 4;
+var yy = $(window).height() / 4;
+var theta = 0;
+var vtheta = Math.PI / 180;
+var r = 200;
+setInterval(function () {
+    $('.earth').css({
+        'left': xx + r * Math.cos(theta),
+        'top': yy + r * Math.sin(theta)
+    });
+    theta = theta + vtheta;
+}, 20);
 
 
